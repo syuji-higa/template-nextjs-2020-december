c7300d99b99e37b66b0826cc9af09faa976f984a
syuji-higa <syuji-higa@gmail.com>
2020-11-19 20:11:55 +0900

    feat: 記述に型などを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
+import { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -63,3 +64,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home
