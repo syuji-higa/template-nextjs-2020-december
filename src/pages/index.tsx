357a297f2f5ade489830a375acdebe35ab7195fa
syuji-higa <syuji-higa@gmail.com>
2020-11-22 06:29:56 +0900

    feat: モジュールインポートのベース URL を設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from 'styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
