6f95b507617768dd7b0d5f8b1f38f01e4ec7566b
syuji-higa <syuji-higa@gmail.com>
2020-11-29 17:15:59 +0900

    feat: 静的解析と整形のツールを追加

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  ignorePatterns: ['!.eslintrc.js'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  semi: false,
+  arrowParens: 'always',
+  singleQuote: true
+}
