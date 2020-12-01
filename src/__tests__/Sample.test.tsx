a9dadb2e9c0cd6f84f91ca943f69feeff880c798
syuji-higa <syuji-higa@gmail.com>
2020-12-01 21:16:25 +0900

    feat: React Testing Library を追加

--- src/__tests__/Sample.test.tsx
+++ src/__tests__/Sample.test.tsx
@@ -1,5 +1,16 @@
+import React from 'react'
+import { cleanup, render, screen } from '@testing-library/react'
 import Home from 'pages/index'
+
+afterEach(cleanup)
 
 it('Home ページコンポーネントが存在している', () => {
   expect(Home).toBeTruthy()
+})
+
+it('「Next.js!」のリンクが Next.js の公式サイトのトップページである', () => {
+  render(<Home />)
+  expect(screen.getByText('Next.js!').getAttribute('href')).toBe(
+    'https://nextjs.org'
+  )
 })
